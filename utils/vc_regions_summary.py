3ea1614f8b2796b9609a68c9a9ecbc7025ba4afc
Hannah <hannahghaffari@gmail.com>
2025-07-23 00:34:15 -0700

    Update path to be more suitable for dev

--- utils/vc_regions_summary.py
+++ utils/vc_regions_summary.py
@@ -1,6 +1,8 @@
 # determine most common visual cortex regions per session
 # figure out which brain regions are frequent and worth analyzing
 
+import os
+from pathlib import Path
 import pandas as pd
 from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProjectCache
 
@@ -8,11 +10,14 @@ from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProj
 import warnings
 warnings.filterwarnings("ignore")
 
-manifest_path = '/Users/hannahghaffari/Documents/regional-visual-responsiveness/ecephys_cache_dir/manifest.json'
+project_root = Path(__file__).resolve().parent.parent
+#print(os.getcwd())
+
+manifest_path = project_root / "ecephys_cache_dir" / "manifest.json"
 cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
 
 sessions = cache.get_session_table()
-session = cache.get_session_data(sessions.index[2]) # pick first for now
+session = cache.get_session_data(sessions.index[0]) # pick first for now
 
 # get all channels info
 channels = session.channels
