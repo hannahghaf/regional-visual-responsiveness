1d269f38e0e732e2b5fc6837410d243c04ad07eb
Hannah <hannahghaffari@gmail.com>
2025-07-22 22:24:56 -0700

    Bug fixes and cleanup; dealing with missing/NA values

--- utils/vc_regions_summary.py
+++ utils/vc_regions_summary.py
@@ -4,14 +4,23 @@
 import pandas as pd
 from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProjectCache
 
+# reduce output noise
+import warnings
+warnings.filterwarnings("ignore")
+
 manifest_path = '/Users/hannahghaffari/Documents/regional-visual-responsiveness/ecephys_cache_dir/manifest.json'
 cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
 
 sessions = cache.get_session_table()
-session = cache.get_session_data(sessions.index[0]) # pick first for now
+session = cache.get_session_data(sessions.index[2]) # pick first for now
 
 # get all channels info
 channels = session.channels
-visual_channels = channels[channels['structure_acronym'].str.startswith('VIS')]
-region_counts = visual_channels['structure_acronym'].value_counts()
+channels = channels.dropna(subset=['ecephys_structure_acronym'])
+visual_channels = channels[channels['ecephys_structure_acronym'].str.startswith('VIS')]
+region_counts = visual_channels['ecephys_structure_acronym'].value_counts()
 print(region_counts.head(10))
+
+# print session ID
+session_id = session.ecephys_session_id
+print(session_id)
