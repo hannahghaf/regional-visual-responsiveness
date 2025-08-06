4bb7d890ebb0283808779e0086f1c3d364b9be3a
Hannah <hannahghaffari@gmail.com>
2025-08-06 14:27:43 -0700

    Schema normalization; add channel_id column

--- bin/compute_spike_rates.py
+++ bin/compute_spike_rates.py
@@ -14,7 +14,7 @@ units = pd.read_csv(data_root / "units.csv")
 # count spikes per unit
 #spike_counts = spikes.groupby("unit").size()
 
-# spike rate = total spikes / total recording time
+# spike rate = total spikes / total recording time=
 #spike_rates = spike_counts / duration
 
 #print("Spike rates... ")
@@ -23,4 +23,12 @@ units = pd.read_csv(data_root / "units.csv")
 # merge spike rates w channel ids CHANGE!!!!
 units_min = units[['id', 'ecephys_channel_id']]
 channels_with_rate = spikes.merge(units_min, left_on="unit", right_on="id", how="left")
-print(channels_with_rate.tail())
+
+#make this production ready ##########################################################
+################# t?
+channels_with_rate.drop("id", axis=1, inplace=True)
+channels_with_rate.rename(columns={'unit':'unit_id', 't':'spike_timestamp'}, inplace=True)
+channels_with_rate = channels_with_rate[['ecephys_channel_id', 'unit_id', 'spike_timestamp']]
+
+print(channels_with_rate.head())
+print(channels_with_rate.columns)
