0ca1a2dac2648309b6b5a13b65e1acef45b9c617
Hannah <hannahghaffari@gmail.com>
2025-08-07 09:36:21 -0700

    Make .pkl file for faster loading and reading of data

--- bin/download_data.py
+++ bin/download_data.py
@@ -5,11 +5,13 @@ from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProj
 
 project_root = Path(__file__).resolve().parent.parent
 data_out = project_root / "data"
+pickle_dir = data_out / "pickle"
+pickle_dir.mkdir(parents=True, exist_ok=True)
 
 cache = EcephysProjectCache.from_warehouse(manifest=os.path.join(data_out, "manifest.json"))
 sessions = cache.get_session_table().index[:5]
-print(data_out)
 
+'''
 for sid in sessions:
     sess = cache.get_session_data(sid)
     path = os.path.join(data_out, f"session_{sid}")
@@ -18,5 +20,5 @@ for sid in sessions:
         {"unit": u, "t": t} for u, ts in sess.spike_times.items() for t in ts
     ]).to_csv(f"{path}/spike_times.csv", index=False)
     sess.channels.to_csv(f"{path}/channels.csv", index=False)
-
+'''
 print("done")
